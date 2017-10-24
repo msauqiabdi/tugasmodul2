04642afb42893938f7e0385671f1ccc0d6d6b113
Muhammad Sauqi Abdi <muhammadsauqiabdi7@gmail.com>
2017-10-24 18:21:18 +0700

    Perbaikan coding lebih enak ini

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -4,21 +4,26 @@ using namespace std;
 
 int main()
 {
-	double lr,p=0.234495,jbp,bp,bpp,a,b,jbph,tb;
-	cout<<"Luas Rumah : ";cin>>lr;
-	cout<<"Jumlah box parket :";cin>>jbp;
-	cout<<""<<endl;
-	cout<<""<<endl;
-	cout<<"Luas Rumah : "<<lr;cout<<" m2"<<endl;
-	cout<<"Luas box parket : "<<p;cout<<" m2"<<endl;
-	a=p*jbp;
-	b=lr/a;
-	jbph=b*103/100;
-	cout<<"jumlah box parket : "<<jbph<<endl;
-	bp=jbph*500;
-	cout<<"Biaya parkat(Rp) : "<<bp<<endl
-	bpp=lr*20;
-	cout<<"Biaya pasang(Rp) : "<<bpp<<endl;
-	tb=bp+bpp;
-	cout<<"Total Biaya (Rp) : "<<tb<<endl;
+	double lr =42;
+	double lp = 193*1215;
+	double l_1_box =lp*8/1000000;
+	double Jbp,bp,bpg,Ttlbyr;
+	
+	cout <<" Luas Rumah :"<<lr<<"m2"<<endl;
+	cout <<" Luas 1 Box Parket : "<<l_1_box<<"m2"<<endl;
+	
+	Jbp = lr/l_1_box;
+	cout <<" Jumlah Box Parket :"<<bp<<endl;
+	
+	bp = Jbp*500;
+	cout <<" Biaya Parket (RP) :"<<bp<<endl;
+	
+	bp = lr*20;
+	cout <<" Biasa Pasang (RP) :"<<bp<<endl;
+	
+	Ttlbyr =bp+bpg;
+	cout <<" Total Biaya (RP) :"<<Ttlbyr<<endl;
+	cout <<" Muhammad Sauqi Abdi ";
+	cout <<" 16.63.0317 ";
+	
 }
